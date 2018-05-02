2214fa0030b25f0776c0e25d485bcc64aaa9b628
Émile <xiaotong-robillard@hotmail.com>
2018-05-01 22:29:38 -0400

    Network

--- TP_BJ/BlackJack/Cartes.cs
+++ TP_BJ/BlackJack/Cartes.cs
@@ -8,19 +8,26 @@ namespace Partie
 {
    public class Cartes
    {
-      string sorte;
-      int type;
-      int valeur;
-      public Cartes(string sorte, int type)
+      string type; // Coeur, Carreaux, Trefle, Pique
+      int figure; // 2..10, J = 11, Q = 12, K = 13, As = 1 ou 11
+      int valeur; // 1..11
+      public Cartes(string type, int figure)
       {
-         this.sorte = sorte;
+
          this.type = type;
+         this.figure = figure;
          valeur = calcValeur();
+      }
+      public Cartes(string type, int figure, int sommeCartes)
+      {
+         this.type = type;
+         this.figure = figure;
+         valeur = (figure == 1 && (sommeCartes + 11) > 21) ? calcValeur() : 11;
       }
 
       public string imgCarte
       {
-         get { return string.Format("{0}_{1}", type, sorte); }
+         get { return string.Format("{0}_{1}.jpg", figure, type); }
       }
 
       public int Valeur
@@ -41,15 +48,15 @@ namespace Partie
       private int calcValeur()
       {
          // Retourne la valeur de la carte selons sont type
-         if (type > 10)
+         if (figure > 10)
          {
             return 10;
          }
-         else if (type == 1)
+         else if (figure == 1)
          {
             return 11;
          }
-         return type;
+         return figure;
       }
    }
 }
--- TP_BJ/BlackJack/Net.cs
+++ TP_BJ/BlackJack/Net.cs
@@ -3,10 +3,159 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
 
 namespace Partie
 {
-   class Net
-   {
-   }
+	class Net
+	{
+		const int Port = 8000;
+		TcpListener Listener1, Listener2, Listener3, Listener4;
+
+		NetworkStream nsClient, nsJ1, nsJ2, nsJ3, nsJ4;
+		Socket socketJ1, socketJ2, socketJ3, socketJ4;
+		StreamReader readJ1, readJ2, readJ3, readJ4;
+		StreamWriter writeJ1, writeJ2, writeJ3, writeJ4;
+		IPAddress AddrIP;
+		string stringIP;
+		int NbJoueur;
+
+		public Net(int nombreJoueur)
+		{
+			NbJoueur = nombreJoueur;
+		}
+		public Net()
+		{
+			NbJoueur = 1;
+		}
+		public string IP
+		{
+			set
+			{
+				stringIP = value;
+				AddrIP = IPAddress.Parse(stringIP);
+			}
+		}
+
+		public void Host()
+		{
+
+			for (int i = 4; i > 0; i--)
+			{
+				switch (i)
+				{
+					case 4:
+						Listener4 = new TcpListener(AddrIP, Port);
+						socketJ4 = Listener4.AcceptSocket();
+						break;
+					case 3:
+						Listener3 = new TcpListener(AddrIP, Port);
+						socketJ3 = Listener3.AcceptSocket();
+						break;
+					case 2:
+						Listener2 = new TcpListener(AddrIP, Port);
+						socketJ2 = Listener2.AcceptSocket();
+						break;
+					case 1:
+						Listener1 = new TcpListener(AddrIP, Port);
+						socketJ1 = Listener1.AcceptSocket();
+						break;
+				}
+			}
+		}
+		public void envoyerTous(string message)
+		{
+			// envoie un message à tous les joueurs
+			try
+			{
+				writeJ1 = new StreamWriter(nsJ1);
+				writeJ1.WriteLine(message);
+				writeJ1.Flush();
+				writeJ2 = new StreamWriter(nsJ2);
+				writeJ2.WriteLine(message);
+				writeJ2.Flush();
+				writeJ3 = new StreamWriter(nsJ3);
+				writeJ3.WriteLine(message);
+				writeJ3.Flush();
+				writeJ4 = new StreamWriter(nsJ4);
+				writeJ4.WriteLine(message);
+				writeJ4.Flush();
+			}
+			catch
+			{
+
+			}
+		}
+		public void envoyerMessage(string message, int IDJoueur)
+		{
+			// envoi une message à une joueur/personne
+			try
+			{
+				switch (IDJoueur)
+				{
+					case 1:
+						writeJ1 = new StreamWriter(nsJ1);
+						writeJ1.WriteLine(message);
+						writeJ1.Flush();
+						break;
+					case 2:
+						writeJ2 = new StreamWriter(nsJ2);
+						writeJ2.WriteLine(message);
+						writeJ2.Flush();
+						break;
+					case 3:
+						writeJ3 = new StreamWriter(nsJ3);
+						writeJ3.WriteLine(message);
+						writeJ3.Flush();
+						break;
+					case 4:
+						writeJ4 = new StreamWriter(nsJ4);
+						writeJ4.WriteLine(message);
+						writeJ4.Flush();
+						break;
+				}
+
+			}
+			catch
+			{
+
+			}
+		}
+
+		public string recevoirMessage(int IDJoueur)
+		{
+			string leMessage = "";
+			try
+			{
+				switch (IDJoueur)
+				{
+					case 1:
+						readJ1 = new StreamReader(nsJ1);
+						leMessage = readJ1.ReadLine();
+						break;
+					case 2:
+						readJ2 = new StreamReader(nsJ2);
+						leMessage = readJ2.ReadLine();
+						break;
+					case 3:
+						readJ3 = new StreamReader(nsJ3);
+						leMessage = readJ3.ReadLine();
+						break;
+					case 4:
+						readJ4 = new StreamReader(nsJ3);
+						leMessage = readJ4.ReadLine();
+						break;
+				}
+			}
+			catch
+			{
+
+			}
+
+			return leMessage;
+		}
+	}
 }
