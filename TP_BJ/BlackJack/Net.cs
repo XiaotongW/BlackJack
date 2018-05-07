320ef8eac5c0b4a8a142b71b30f0270578c5a6b4
unknown <xiaotong-robillard@hotmail.com>
2018-05-07 13:59:12 -0400

    Net

--- TP_BJ/BlackJack/Net.cs
+++ TP_BJ/BlackJack/Net.cs
@@ -13,21 +13,59 @@ namespace Partie
 	class Net
 	{
 		const int Port = 8000;
-		TcpListener listener;
+		static TcpListener listener;
 		TcpClient client;
 		NetworkStream nsFlux;
 		Socket socketClient;
-		StreamReader readJ2, readJ3, readJ4;
-		StreamWriter writeJ2, writeJ3, writeJ4;
+		//StreamReader readJ2, readJ3, readJ4;
+		//StreamWriter writeJ2, writeJ3, writeJ4;
+		StreamReader reader;
+		StreamWriter writer;
 		IPAddress AddrIP;
 		string stringIP;
 		int NbJoueur;
 		bool IsHost;
+		int IDJoueur;
 
-		public Net(bool IsHost, int IDJoueurs)
+		public Net(bool IsHost, int nbJoueurs)
 		{
 			listener = new TcpListener(IPAddress.Any, Port);
+			listener.Start();
+			socketClient = listener.AcceptSocket();
+			if (socketClient.Connected)
+			{
+				nsFlux = new NetworkStream(socketClient.Accept());
+				reader = new StreamReader(nsFlux);
+				writer = new StreamWriter(nsFlux);
+			}
 
+			this.IsHost = IsHost;
+			NbJoueur = nbJoueurs;
+		}
+		public Net()
+		{
+
+			try
+			{
+				client = new TcpClient(stringIP, Port);
+				IsHost = false;
+				nsFlux = client.GetStream();
+				reader = new StreamReader(nsFlux);
+				writer = new StreamWriter(nsFlux);
+
+			}
+			catch (Exception e)
+			{
+
+				MessageBox.Show(e.ToString());
+			}
+
+		}
+
+		public int ID
+		{
+			set { IDJoueur = value; }
+			get { return IDJoueur; }
 		}
 
 		public string IP
@@ -41,52 +79,23 @@ namespace Partie
 
 		public void Host(int nbJoueurs)
 		{
-			for (int i = 2; i <= nbJoueurs; i++)
-			{
-				listener.Start();
-				client = listener.AcceptTcpClient();
-				switch (i)
-				{
-					case 2:
-						writeJ2 = new StreamWriter(nsFlux);
-						readJ2 = new StreamReader(nsFlux);
-						break;
-					case 3:
-						writeJ3 = new StreamWriter(nsFlux);
-						readJ3 = new StreamReader(nsFlux);
-						break;
-					case 4:
-						writeJ4 = new StreamWriter(nsFlux);
-						readJ4 = new StreamReader(nsFlux);
-						break;
-				}
-			}
+
 		}
 
 		public void envoyerMessage(string message, int IDJoueur)
 		{
-			// envoi une message à une joueur/personne
 			try
 			{
-				switch (IDJoueur)
-				{
-					case 2:
-						writeJ2.WriteLine(message);
-						writeJ2.Flush();
-						break;
-					case 3:
-						writeJ3.WriteLine(message);
-						writeJ3.Flush();
-						break;
-					case 4:
-						writeJ4.WriteLine(message);
-						writeJ4.Flush();
-						break;
-				}
+				writer.WriteLine("{0},{1}", IDJoueur, message);
 			}
-			catch
+			catch (Exception e)
 			{
-
+				MessageBox.Show(e.ToString());
+				writer.Close();
+			}
+			finally
+			{
+				writer.Flush();
 			}
 		}
 
@@ -95,22 +104,12 @@ namespace Partie
 			string leMessage = "";
 			try
 			{
-				switch (IDJoueur)
-				{
-					case 2:
-						leMessage = readJ2.ReadLine();
-						break;
-					case 3:
-						leMessage = readJ3.ReadLine();
-						break;
-					case 4:
-						leMessage = readJ4.ReadLine();
-						break;
-				}
+				leMessage = reader.ReadLine();
 			}
-			catch
+			catch (Exception e)
 			{
-
+				MessageBox.Show(e.ToString());
+				reader.Close();
 			}
 
 			return leMessage;
